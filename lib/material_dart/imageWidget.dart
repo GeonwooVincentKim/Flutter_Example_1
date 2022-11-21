b723f71caba3a9a641c642b40582b52acff5fe54
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-21 17:08:28 +0900

    [Feature] - Upload image

---
+++ lib/material_dart/imageWidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class ImageWidgetApp extends StatefulWidget {
+  const ImageWidgetApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ImageWidgetAppState();
+  }
+}
+
+class ImageWidgetAppState extends State<ImageWidgetApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Image Widget')),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('image/sample_image.png', width: 200, height: 100, fit: BoxFit.fill)
+            ],
+          )
+        )
+      )
+    );
+  }
+}
