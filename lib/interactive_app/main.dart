129a82ce3d720f3ef6807397b61b5820dbfefae4
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-22 16:47:44 +0900

    [Update] - Main.dart

--- lib/interactive_app/main.dart
+++ lib/interactive_app/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: _title,
       home: WidgetApp()
     );
@@ -22,6 +22,10 @@ class WidgetApp extends StatefulWidget {
 }
 
 class WidgetAppState extends State<WidgetApp> {
+  String sum = '';
+  TextEditingController value1 = TextEditingController();
+  TextEditingController value2 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,17 +35,35 @@ class WidgetAppState extends State<WidgetApp> {
           child: Column(
             // ignore: prefer_const_literals_to_create_immutables
             children: [
-              const Padding(
-                padding: EdgeInsets.all(15),
-                child: Text('Flutter')
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextField(keyboardType: TextInputType.number, controller: value1)
               ),
-              const Padding(
-                padding: EdgeInsets.only(left: 20, right: 20),
-                child: TextField(),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: TextField(keyboardType: TextInputType.number, controller: value2,),
               ),
               Padding(
                 padding: const EdgeInsets.all(15),
-                child: ElevatedButton(child: const Text(''), onPressed: () {})  
+                child: ElevatedButton(
+                  // ignore: sort_child_properties_last
+                  child: Row(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      const Icon(Icons.add),
+                      const Text('더하기')
+                    ],
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.amber)
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      int result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                      sum = '$result';
+                    });
+                  }
+                )  
               )
             ],
           ),
