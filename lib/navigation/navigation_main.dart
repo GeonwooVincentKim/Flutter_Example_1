efc1197ec7dedfa53328081937c15290c602941c
GeonwooVincentKim <kdsnop@gmail.com>
2022-11-26 21:23:35 +0000

    [Feature] - Navigation Main and Navigation Main Error.dart

---
+++ lib/navigation/navigation_main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "SubPage Example",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sub Page Main')),
+      body: Container(
+        child: Center(
+          child: Text('First Page')
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  } 
+}
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+  
+  @override
+  State<StatefulWidget> createState() => SecondPageState();
+}
+
+class SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sub Page Main')),
+      body: Container(
+        child: Center(
+          child: Text('First Page')
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  } 
+}
