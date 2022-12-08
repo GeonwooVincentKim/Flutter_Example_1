c2a24039806208b26809a7daa744dd63aecfe308
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-08 17:28:58 +0900

    [Update] - Second Page.dart

--- lib/list_view_example/screen/second_page.dart
+++ lib/list_view_example/screen/second_page.dart
@@ -25,14 +25,6 @@ class _SecondAppState extends State<SecondApp> {
   late Animal animal;
 
   @override
-  void initState() {
-    super.initState();
-
-    // animalList = AnimalProvider().animalItems;
-    // animalImage = Animal.from(animal).imagePath;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
@@ -63,20 +55,60 @@ class _SecondAppState extends State<SecondApp> {
                   onChanged: (bool? check) => setState(() => flyExist = check!))
             ],
           ),
-          Row(
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const ImageGestureDetectore(
-                  width: 80, imagePath: "image/chapter_06/cow.png"),
-              const ImageGestureDetectore(
-                  width: 80, imagePath: "image/chapter_06/pig.png"),
-              const ImageGestureDetectore(
-                  width: 80, imagePath: "image/chapter_06/bee.png"),
-              const ImageGestureDetectore(
-                  width: 80, imagePath: "image/chapter_06/cat.png"),
-              const ImageGestureDetectore(
-                  width: 80, imagePath: "image/chapter_06/dog.png"),
-            ],
+          Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const ImageGestureDetectore(
+                      width: 80, imagePath: "image/chapter_06/cow.png"),
+                  const ImageGestureDetectore(
+                      width: 80, imagePath: "image/chapter_06/pig.png"),
+                  const ImageGestureDetectore(
+                      width: 80, imagePath: "image/chapter_06/bee.png"),
+                  const ImageGestureDetectore(
+                      width: 80, imagePath: "image/chapter_06/cat.png"),
+                  const ImageGestureDetectore(
+                      width: 80, imagePath: "image/chapter_06/dog.png"),
+                ],
+              )),
+          ElevatedButton(
+            child: const Text('Add Animal'),
+            onPressed: () {
+              var animal = Animal(
+                  animalName: nameController.value.text,
+                  kind: getKind(_radioValue),
+                  imagePath: _imagePath,
+                  flyExist: flyExist);
+
+              AlertDialog dialog = AlertDialog(
+                title: const Text('Add Animal'),
+                content: Text(
+                  // ignore: prefer_interpolation_to_compose_strings
+                  'This is ${animal.animalName}.' +
+                      'The Kind is ${animal.kind}\n' +
+                      'Do you want to add this animal?',
+                  style: const TextStyle(fontSize: 30.0),
+                ),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        widget.list.add(animal);
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Yes')),
+                  ElevatedButton(
+                      onPressed: () {
+                        widget.list.add(animal);
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('No')),
+                ],
+              );
+              showDialog(
+                  context: context, builder: (BuildContext context) => dialog);
+            },
           ),
         ],
       )),
@@ -97,9 +129,20 @@ class _SecondAppState extends State<SecondApp> {
 
   // }
 
-  void _radioChange(int? value) {
+  _radioChange(int? value) {
     setState(() {
       _radioValue = value!;
     });
+  }
+
+  getKind(int radioValue) {
+    switch (radioValue) {
+      case 0:
+        return "양서류";
+      case 1:
+        return "파충류";
+      case 2:
+        return "포유류";
+    }
   }
 }
