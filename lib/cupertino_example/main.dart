32478d9fb92b4314f2363e06a226727613bbd17e
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-14 14:50:25 +0900

    [Update]

--- lib/cupertino_example/main.dart
+++ lib/cupertino_example/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1_1/cupertino_example/cupertino_main.dart';
 
 void main() => runApp(const CupertinoMain());
