ad0c088a9cb8f4e8d4faa9c230c5422082a33f25
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-16 15:28:20 +0900

    [Update] - Body.dart has been updated

--- lib/cupertino_button/body.dart
+++ lib/cupertino_button/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1_1/cupertino_button/widget/cupertino_navigation_widget.dart';
+import 'package:flutter_application_1_1/cupertino_example/cupertino_main_new_example.dart';
 
 class Body extends StatefulWidget {
   const Body({super.key});
@@ -25,43 +26,68 @@ class _BodyState extends State<Body> {
           ]
       ),
       tabBuilder: (context, index) {
-        late final CupertinoTabView returnValue;
         List<String> getTitleText = ['Calls', 'Chats', 'Settings'];
 
         switch (index) {
-          case 0:
-            returnValue = CupertinoTabView(builder: (context) {
-              return CupertinoNavigationWidget(
-                  middleWidget: Text(getTitleText[0]), 
-                  titleText: getTitleText[0]
-                );
-            });
-            break;
-          
+          case 0:   
+            return printValue(getTitleText[0]);
           case 1:
-            returnValue = CupertinoTabView(
-              builder: (context) {
-                return CupertinoNavigationWidget(
-                  middleWidget: Text(getTitleText[1]), 
-                  titleText: getTitleText[1]
-                );
-              },
-            );
-            break;
-          
+            return printValue(getTitleText[1]);
           case 2:
-            returnValue = CupertinoTabView(
-              builder: (context) {
-                return CupertinoNavigationWidget(
-                  middleWidget: Text(getTitleText[2]), 
-                  titleText: getTitleText[2]
-                );
-              },
-            );
-            break;
+            return printValue(getTitleText[2]);
         }
 
-        return returnValue;
+        return printValue(getTitleText[0]);
+      },
+      // tabBuilder: (context, index) {
+      //   late CupertinoTabView returnValue;
+      //   List<String> getTitleText = ['Calls', 'Chats', 'Settings'];
+
+      //   switch (index) {
+      //     case 0:
+      //       returnValue = CupertinoTabView(builder: (context) {
+      //         return CupertinoNavigationWidget(
+      //             middleWidget: Text(getTitleText[0]), 
+      //             titleText: getTitleText[0]
+      //           );
+      //       });
+      //       break;
+          
+      //     case 1:
+      //       returnValue = CupertinoTabView(
+      //         builder: (context) {
+      //           return CupertinoNavigationWidget(
+      //             middleWidget: Text(getTitleText[1]), 
+      //             titleText: getTitleText[1]
+      //           );
+      //         },
+      //       );
+      //       break;
+          
+      //     case 2:
+      //       returnValue = CupertinoTabView(
+      //         builder: (context) {
+      //           return CupertinoNavigationWidget(
+      //             middleWidget: Text(getTitleText[2]), 
+      //             titleText: getTitleText[2]
+      //           );
+      //         },
+      //       );
+      //       break;
+      //   }
+
+      //   return returnValue;
+      // },
+    );
+  }
+
+  CupertinoTabView printValue (textLocation) {
+    return CupertinoTabView(
+      builder: (context) {
+        return CupertinoNavigationWidget(
+          middleWidget: Text(textLocation),
+          titleText: textLocation
+        );
       },
     );
   }
