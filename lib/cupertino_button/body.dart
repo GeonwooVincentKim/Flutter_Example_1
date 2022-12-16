ead4b62036b1b74b52b9c14ac7bb0d54c65cefc1
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-16 16:06:38 +0900

    [Update] - Cupertino.dart

--- lib/cupertino_button/body.dart
+++ lib/cupertino_button/body.dart
@@ -38,46 +38,7 @@ class _BodyState extends State<Body> {
         }
 
         return printValue(getTitleText[0]);
-      },
-      // tabBuilder: (context, index) {
-      //   late CupertinoTabView returnValue;
-      //   List<String> getTitleText = ['Calls', 'Chats', 'Settings'];
-
-      //   switch (index) {
-      //     case 0:
-      //       returnValue = CupertinoTabView(builder: (context) {
-      //         return CupertinoNavigationWidget(
-      //             middleWidget: Text(getTitleText[0]), 
-      //             titleText: getTitleText[0]
-      //           );
-      //       });
-      //       break;
-          
-      //     case 1:
-      //       returnValue = CupertinoTabView(
-      //         builder: (context) {
-      //           return CupertinoNavigationWidget(
-      //             middleWidget: Text(getTitleText[1]), 
-      //             titleText: getTitleText[1]
-      //           );
-      //         },
-      //       );
-      //       break;
-          
-      //     case 2:
-      //       returnValue = CupertinoTabView(
-      //         builder: (context) {
-      //           return CupertinoNavigationWidget(
-      //             middleWidget: Text(getTitleText[2]), 
-      //             titleText: getTitleText[2]
-      //           );
-      //         },
-      //       );
-      //       break;
-      //   }
-
-      //   return returnValue;
-      // },
+      }
     );
   }
 
