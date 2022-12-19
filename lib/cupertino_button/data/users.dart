935ddeaafb6a5fd6026810b914182a4f894b3c57
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-19 11:48:15 +0900

    [Update] - Color section

--- lib/cupertino_button/data/users.dart
+++ lib/cupertino_button/data/users.dart
@@ -1,21 +1,20 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_application_1_1/cupertino_button/model/users.dart';
+import 'package:flutter_application_1_1/cupertino_button/shared/style.dart';
 
 List<User> users = [
-  User(name: 'Jackname: ', color: CupertinoColors.systemGreen),
-  User(name: 'Lucyname: ', color: Colors.green),
-  User(name: 'Lunaname: ', color: Colors.black26),
-  User(name: 'Olivername: ', color: Colors.blue),
-  User(name: 'Lilyname: ', color: Colors.amberAccent),
-  User(name: 'Miloname: ', color: Colors.purple),
-  User(name: 'Maxname: ', color: Colors.pink),
-  User(name: 'Kittyname: ', color: Colors.yellowAccent),
-  User(name: 'Simbaname: ', color: Colors.red),
-  User(name: 'Zoename: ', color: Colors.blueAccent),
-  User(name: 'Jaspername: ', color: Colors.deepOrange),
-  User(name: 'Stellaname: ', color: Colors.cyan),
-  User(name: 'Lolaname: ', color: Colors.lightBlue),
-  User(name: 'Halseyname: ', color: Colors.deepPurpleAccent),
-  User(name: 'Taylorname: ', color: Colors.indigoAccent),
+  User(name: 'Jackname: ', color: greenAccent),
+  User(name: 'Lucyname: ', color: green),
+  User(name: 'Lunaname: ', color: black26),
+  User(name: 'Olivername: ', color: blue),
+  User(name: 'Lilyname: ', color: amberAccent),
+  User(name: 'Miloname: ', color: purple),
+  User(name: 'Maxname: ', color: pink),
+  User(name: 'Kittyname: ', color: yellowAccent),
+  User(name: 'Simbaname: ', color: red),
+  User(name: 'Zoename: ', color: blueAccent),
+  User(name: 'Jaspername: ', color: deepOrange),
+  User(name: 'Stellaname: ', color: cyan),
+  User(name: 'Lolaname: ', color: lightBlue),
+  User(name: 'Halseyname: ', color: deepPurpleAccent),
+  User(name: 'Taylorname: ', color: indigoAccent),
 ];
