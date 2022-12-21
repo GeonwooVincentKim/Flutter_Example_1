5c81284ddd73b649271a34bdd46472af982f5cc4
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-21 10:25:15 +0900

    [Feature] - Cupertino TabView PrintValue has just sepearted

--- lib/cupertino_button/widgets/cupertino_tabview_printvalue.dart
+++ lib/cupertino_button/widgets/cupertino_tabview_printvalue.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_1_1/cupertino_button/app_screens/cupertino_chat.dart';
 import 'package:flutter_application_1_1/cupertino_button/model/users.dart';
+import 'package:flutter_application_1_1/cupertino_button/widgets/cupertino_scroll_view/cupertino_sliver_navi_bar.dart';
 
 class PrintValue extends StatelessWidget {
   final dynamic textLocation;
@@ -18,21 +19,7 @@ class PrintValue extends StatelessWidget {
       builder: (context) {
         return CustomScrollView(
           slivers: [
-            CupertinoSliverNavigationBar(
-              largeTitle: Text(textLocation),
-              leading: const Text(
-                'Edit',
-                style: TextStyle(color: CupertinoColors.link),
-              ),
-              middle: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  CupertinoActivityIndicator(),
-                  SizedBox(width: 8),
-                  Text('Waiting for Network')
-                ],
-              )
-            ),
+            CupertinoSliverNaviBar(textLocation: textLocation),
             CupertinoChatPage(userList: userList),
           ],
         );
