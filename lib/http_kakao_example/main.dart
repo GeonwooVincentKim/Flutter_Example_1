2b14bb3f7163ba1fbf4cd4a2e35687f2195a1a9e
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-26 10:51:13 +0900

    [Feature] - Parse html codes from google

---
+++ lib/http_kakao_example/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  const HttpApp({super.key});
+
+  @override
+  State<HttpApp> createState() => _HttpAppState();
+}
+
+class _HttpAppState extends State<HttpApp> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Http Example')),
+      body: Container(
+        child: Center(
+          child: Text('$result')
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var url = 'http://www.google.com';
+          var response = await http.get(Uri.parse(url));
+          
+          setState(() {
+            result = response.body;
+          });
+        },
+        child: const Icon(Icons.file_download)
+      ),
+    );
+  }
+}
