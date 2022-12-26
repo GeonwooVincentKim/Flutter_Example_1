aab17d0e7b8ae81f0a7aeb8e8814ee2db6231e8b
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-26 14:57:15 +0900

    [Update] - LargeFile

--- lib/http_kakao_example/app_screens/largefile_main.dart
+++ lib/http_kakao_example/app_screens/largefile_main.dart
@@ -15,14 +15,30 @@ class _LargeFileMainState extends State<LargeFileMain> {
     'https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg'
     '?auto=compress';
 
+  TextEditingController? _editingController;
   bool downloading = false;
   var progressString = "";
   String file = "";
 
   @override
+  void initState() {
+    super.initState();
+    _editingController = new TextEditingController(
+      text: 'https://images.pexels.com/photos/240040/pexels-photo-240040.jpeg?auto=compress'
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Large File Example')),
+      appBar: AppBar(
+        title: TextField(
+          controller: _editingController,
+          style: const TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(hintText: 'Type URL'),
+        )
+      ),
       body: Center(
         child: downloading
           ? Container(
@@ -42,7 +58,32 @@ class _LargeFileMainState extends State<LargeFileMain> {
                 ]
               )
             )
-          ) : Container()
+          ) : FutureBuilder(
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  print('None');
+                  return Text("No Data");
+                case ConnectionState.waiting:
+                  print('Waiting');
+                  return CircularProgressIndicator();
+                case ConnectionState.active:
+                  print('Active');
+                  return CircularProgressIndicator();
+                case ConnectionState.done:
+                  print('Done');
+                  
+                  if (snapshot.hasData) {
+                    return snapshot.data as Widget;
+                  }
+              }
+
+              print('End Process');
+              return Text('No Data');
+            },
+
+            future: downloadWidget(file),
+          )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -58,7 +99,7 @@ class _LargeFileMainState extends State<LargeFileMain> {
 
     try {
       var dir = await getApplicationDocumentsDirectory();
-      await dio.download(imgUrl, '${dir.path}/myimage.jpg',
+      await dio.download(_editingController!.value.text, '${dir.path}/myimage.jpg',
         onReceiveProgress: (rec, total) {
           print('Rec: $rec, Total: $total');
           
@@ -79,5 +120,21 @@ class _LargeFileMainState extends State<LargeFileMain> {
     });
 
     print('Download completed');
+  }
+
+  Future<Widget> downloadWidget(String filePath) async {
+    File file = File(filePath);
+    bool exist = await file.exists();
+    new FileImage(file).evict();
+
+    if (exist) {
+      return Center(
+        child: Column(
+          children: [
+            Image.file(File(filePath))
+          ],
+        )
+      );
+    } else return Text('No Data');
   }
 }
