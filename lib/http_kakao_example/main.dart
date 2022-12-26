7bf85beec6c40b8f2cee3ee36961ec251064a56d
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-26 11:27:27 +0900

    [Update] - Separated two files

--- lib/http_kakao_example/main.dart
+++ lib/http_kakao_example/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_application_1_1/http_kakao_example/app_screens/http_app.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,102 +11,5 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: HttpApp(),
     );
-  }
-}
-
-class HttpApp extends StatefulWidget {
-  const HttpApp({super.key});
-
-  @override
-  State<HttpApp> createState() => _HttpAppState();
-}
-
-class _HttpAppState extends State<HttpApp> {
-  String result = '';
-  List? data;
-
-  @override
-  void initState() {
-    super.initState();
-    data = new List.empty(growable: true);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Http Example')),
-      body: Center(
-        child: data!.isEmpty
-          ? const Text(
-            'No Data',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ) : 
-          ListView.builder(
-            itemBuilder: (context, index) {
-              return Card(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      data![index]['thumbnail'],
-                      height: 100,
-                      width: 100,
-                      fit: BoxFit.contain
-                    ),
-                    Column(
-                      children: [
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width - 150,
-                          child: Text(
-                            data![index]['title'].toString(),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Text('Author : ${data![index]['authors'].toString()}'),
-                        Text('Price : ${data![index]['sale_price'].toString()}'),
-                        Text('Sell Status : ${data![index]['status'].toString()}'),
-                      ],
-                    )
-                  ],
-                )
-              );
-            },
-            itemCount: data!.length,
-          )
-      ),
-      floatingActionButton: FloatingActionButton(
-        // onPressed: () async {
-        //   var url = 'http://www.google.com';
-        //   var response = await http.get(Uri.parse(url));
-          
-        //   setState(() {
-        //     result = response.body;
-        //   });
-        // },
-        onPressed: () async {
-          getJSONData();
-        },
-        child: const Icon(Icons.file_download)
-      ),
-    );
-  }
-
-  Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
-    var response = await http.get(
-      Uri.parse(url),
-      headers: {
-        "Authorization": "KakaoAK 4ef0066672e401dbdd94650e01ef9879"
-      }
-    );
-
-    setState(() {
-      var dataConvertedToJSON = json.decode(response.body);
-      List result = dataConvertedToJSON['documents'];
-      data!.addAll(result);
-    });
-
-    return response.body;
   }
 }
