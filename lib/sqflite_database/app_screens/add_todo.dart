1b3456f545294cd1180bbfe20a3aa765a337748f
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-07 23:23:23 +0000

    [Update] - Database feature just upgraded

--- lib/sqflite_database/app_screens/add_todo.dart
+++ lib/sqflite_database/app_screens/add_todo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1_1/sqflite_database/model/todo.dart';
 import 'package:flutter_application_1_1/sqflite_database/widgets/padding_textfield.dart';
+import 'package:sqflite/sqflite.dart';
 
 class AddTodoApp extends StatefulWidget {
   const AddTodoApp({super.key});
