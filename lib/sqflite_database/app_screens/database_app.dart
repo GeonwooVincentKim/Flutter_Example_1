27642757df539533ff5904fa06adba68e1519554
GeonwooVincentKim <kdsnop@gmail.com>
2023-01-10 16:46:51 +0900

    [Feature] - Database app.dart

--- lib/sqflite_database/app_screens/database_app.dart
+++ lib/sqflite_database/app_screens/database_app.dart
@@ -16,7 +16,7 @@ class _DatabaseAppState extends State<DatabaseApp> {
   @override
   void initState() {
     super.initState();
-    // todoList = getTodos();
+    todoList = getTodos();
   }
   
   void _insertTodo(Todo todo) async {
@@ -26,16 +26,74 @@ class _DatabaseAppState extends State<DatabaseApp> {
       conflictAlgorithm: ConflictAlgorithm.replace
     );
 
-    // setState(() {
-    //   todoList = getTodos();
-    // });
+    setState(() {
+      todoList = getTodos();
+    });
+  }
+
+  Future<List<Todo>> getTodos() async {
+    final Database database = await widget.db;
+    final List<Map<String, dynamic>> maps = await database.query('todos');
+
+    return List.generate(maps.length, (i) {
+      int active = maps[i]['active'] == 1 ? 1 : 0;
+      return Todo(
+        title: maps[i]['title'].toString(),
+        content: maps[i]['content'].toString(),
+        active: active,
+        id: maps[i]['id']
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Database Example')),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:  return CircularProgressIndicator();
+                case ConnectionState.waiting:   return CircularProgressIndicator();
+                case ConnectionState.active:  return CircularProgressIndicator();
+                case ConnectionState.done:
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        Todo todo = (snapshot.data as List<Todo>)[index];
+
+                        return ListTile(
+                          title: Text(
+                            todo.title!,
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                          subtitle: Column(
+                            children: [
+                              Text(todo.content!),
+                              Text('Check: ${todo.active == 1 ? 'true': 'false'}'),
+                              Container(
+                                height: 1,
+                                color: Colors.blue
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: (snapshot.data as List<Todo>).length,
+                    );
+                  }
+                else {
+                  return const Text('No Data');
+                }
+              }
+              return CircularProgressIndicator();
+            },
+            future: todoList,
+          )
+        )
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           final todo = await Navigator.of(context).pushNamed('/add');
